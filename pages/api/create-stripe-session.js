78745b371cadb1eda7ab52061b4c5427de77026d
Filip Johansen <hello@filipjohansen.com>
2022-02-14 08:35:51 +0100

    updated redirect url in CreateStripeSession

--- pages/api/create-stripe-session.js
+++ pages/api/create-stripe-session.js
@@ -21,7 +21,7 @@ async function CreateStripeSession(req, res) {
   const redirectURL =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://frend.vercel.app";
+      : "https://filipjohnxfrend.vercel.app/";
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
